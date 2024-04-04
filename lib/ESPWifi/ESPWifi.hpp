40577aa2c837dd757af590ba0f62a1b9d5f5b1e4
Lirae Que Data <liraedata59@gmail.com>
2024-04-04 13:14:51 +0800

    Add constructor in ESPWifi.hpp

--- lib/ESPWifi/ESPWifi.hpp
+++ lib/ESPWifi/ESPWifi.hpp
@@ -2,9 +2,16 @@
 #define ESPWIFI_HPP
 
 
+#include <Arduino.h>
+#include <WiFi.h>
+#include <Update.h>
+#include <LittleFS.h>
+#include <SD.h>
+
+
 class ESPWifi {
     public:
-
+        ESPWifi(const String ssid, const String password);
 };
 
 
