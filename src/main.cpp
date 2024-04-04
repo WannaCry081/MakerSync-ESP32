8d751a9fb4192f3ea8ef85b76b705168087b8286
Lirae Que Data <liraedata59@gmail.com>
2024-04-04 13:42:10 +0800

    Add ESPWifi in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <ESPWifi.hpp>
 
+
 const String WIFI_SSID = "";
 const String WIFI_PASSWORD = "";
 
@@ -11,12 +12,35 @@ const byte POTENTIOMETER = 26;
 const byte MOTOR = 25;
 
 
+ESPWifi wifi(WIFI_SSID, WIFI_PASSWORD);
+
+
 void setup(){
 
+    Serial.begin(115200);
 
+    Serial.printf("Connecting to %s", WIFI_SSID);
+    while (!wifi.isConnect()) {
+        Serial.print(".");
+        delay(1000);
+
+        if (!wifi.isSuccess()){
+            Serial.printf("\nError! Failed to Connect to%s.\n", WIFI_SSID);
+        }
+    }
+
+    if (wifi.isConnect()){
+        Serial.printf("\nSuccessfully Connected to %s\n", WIFI_SSID);
+        wifi.displayStatus();
+    }
 }
 
 void loop(){
 
+    if (!wifi.isConnect()){
+        return;
+    }
+
+    
 
 }
