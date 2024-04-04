ec72818b1b85339f53b015c13e10ecf0155e182b
Lirae Que Data <liraedata59@gmail.com>
2024-04-04 13:08:37 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+const int btn_start = 13;
+const int btn_stop = 12;
+const int led_green = 14;
+const int led_red = 27;
+// const int potentiometer = 26;
+// const int motor = 25;
+
+
+void setup(){
+    
+
+
+void loop(){
+
+
+}
