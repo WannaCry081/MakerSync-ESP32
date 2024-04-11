fbbcc16ceaa080d776220abb5be21020383a205a
Lirae Que Data <liraedata59@gmail.com>
2024-04-12 02:56:00 +0800

    Update pins value in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,11 @@
 #include <max6675.h>
 
 
-const String WIFI_SSID = "";
-const String WIFI_PASSWORD = "";
-
-const byte BTN_START = 13;
-const byte BTN_STOP = 12;
-const byte LED_GREEN = 14;
-const byte LED_RED = 27;
-const byte POTENTIOMETER = 26;
+const byte BTN_START = 19;
+const byte BTN_STOP = 5;
+const byte LED_GREEN = 12;
+const byte LED_RED = 13;
+const byte POTENTIOMETER = 35;
 const byte MOTOR = 25;
 const byte SCK_PIN = 14;
 const byte CS_PIN = 27;
