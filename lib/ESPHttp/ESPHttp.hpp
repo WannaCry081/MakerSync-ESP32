9520a66ae2a69d950198c0fe29e96323879e51b2
Lirae Que Data <liraedata59@gmail.com>
2024-04-12 12:41:11 +0800

    Add ESPHttp class in ESPHttp.hpp

---
+++ lib/ESPHttp/ESPHttp.hpp
@@ -0,0 +1,14 @@
+#ifndef ESPHTTP_HPP
+#define ESPHTTP_HPP
+
+
+class ESPHttp {
+    private:
+        String _base_url = "https://maker-sync-django.vercel.app/api/v1/";
+
+    public:
+    
+};
+
+
+#endif
