b4b9165fc95a2e58c6fb5050d88001197c920990
Lirae Que Data <liraedata59@gmail.com>
2024-04-12 16:20:49 +0800

    Update updateSensors method implementation

--- lib/ESPHttp/ESPHttp.cpp
+++ lib/ESPHttp/ESPHttp.cpp
@@ -46,5 +46,24 @@ bool ESPHttp::createSensors(String sensor_id) {
 
 
 bool ESPHttp::updateSensors(String sensor_id, Sensor sensor) {
+    HTTPClient http;
 
+    String endpoint = _sensors_url + sensor_id;
+
+    http.addHeader("Content-Type", "application/json");
+
+    String payload = "{";
+    payload += "\"is_start\":\"" + String(sensor.is_start) + "\", ";
+    payload += "\"is_stop\":\"" + String(sensor.is_stop) + "\", ";
+    payload += "\"is_initialized\":\"" + String(sensor.is_initialized) + "\", ";
+    payload += "\"counter\":\"" + String(sensor.counter) + "\", ";
+    payload += "\"timer\":\"" + String(sensor.timer) + "\", ";
+    payload += "\"temperature\":\"" + String(sensor.temperature) + "\"";
+    payload += "}";
+
+    http.begin(endpoint);
+    int httpResponseCode = http.PUT(payload);
+
+    http.end();
+    return (httpResponseCode == 200) ? true : false;
 }  
