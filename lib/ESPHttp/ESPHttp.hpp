d1b39fc83124273faeaf4496411f8bda2c62e37c
Lirae Que Data <liraedata59@gmail.com>
2024-04-12 12:51:44 +0800

    Create sensor struct in ESPHttp.hpp

--- lib/ESPHttp/ESPHttp.hpp
+++ lib/ESPHttp/ESPHttp.hpp
@@ -2,12 +2,27 @@
 #define ESPHTTP_HPP
 
 
+#include <Arduino.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+
+struct Sensor {
+    boolean is_start; 
+    boolean is_stop;
+    boolean is_initialized;
+    int counter;
+    int timer; 
+    float temperature; 
+};
+
+
 class ESPHttp {
     private:
         String _base_url = "https://maker-sync-django.vercel.app/api/v1/";
 
     public:
-    
+        Sensor retrieveSensors(String sensor_id);
 };
 
 
