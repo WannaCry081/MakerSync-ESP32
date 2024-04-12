8d324cbebd2e465a8fa2ccf140271a18edf50be3
Lirae Que Data <liraedata59@gmail.com>
2024-04-12 13:00:28 +0800

    Add createSensors in ESPHttp.cpp

--- lib/ESPHttp/ESPHttp.cpp
+++ lib/ESPHttp/ESPHttp.cpp
@@ -6,3 +6,11 @@ Sensor ESPHttp::retrieveSensors(String sensor_id) {
 }   
 
 
+void ESPHttp::createSensors(String sensor_id) {
+
+}
+
+
+void ESPHttp::updateSensors(String sensor_id, Sensor sensor) {
+
+}  
