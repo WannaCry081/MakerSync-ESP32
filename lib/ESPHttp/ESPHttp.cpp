dbbd492c43ce42d09451ea63e5d69d9bb0fe557d
Lirae Que Data <liraedata59@gmail.com>
2024-04-12 13:15:00 +0800

    Update retrieveSensors implementation in ESPHttp.cpp

--- lib/ESPHttp/ESPHttp.cpp
+++ lib/ESPHttp/ESPHttp.cpp
@@ -2,7 +2,30 @@
 
 
 Sensor ESPHttp::retrieveSensors(String sensor_id) {
-    
+    Sensor sensor;
+    HTTPClient http;
+
+    String url = _sensors_url + sensor_id;
+    http.begin(url);
+
+    int httpCode = http.GET();
+
+    if (httpCode == 200) {
+        String payload = http.getString();
+
+        JsonDocument doc;
+        deserializeJson(doc, payload);
+
+        sensor.is_start = (doc["is_start"].as<JsonInteger>() != 0);
+        sensor.is_stop = (doc["is_stop"].as<JsonInteger>() != 0);
+        sensor.is_initialized = (doc["is_initialized"].as<JsonInteger>() != 0);
+        sensor.counter = doc["counter"].as<JsonInteger>();
+        sensor.timer = doc["timer"].as<JsonInteger>();;
+        sensor.temperature = doc["temperature"].as<JsonFloat>();
+    }
+
+    http.end();
+    return sensor;
 }   
 
 
