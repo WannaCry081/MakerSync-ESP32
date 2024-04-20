e782feb95505b78e4530e7b7c7e2db89e0ca6ee5
Lirae Que Data <liraedata59@gmail.com>
2024-04-21 01:10:59 +0800

    Update loop method in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,15 @@
 
 // Define pin mappings
 const byte LED_RED = 13;
-const byte LED_GREEN = 12;
-const byte BTN_START = 18;
-const byte BTN_STOP = 5;
+const byte LED_YELLOW = 12; 
+const byte LED_GREEN = 14;
+const byte BTN_START = 19;
+const byte BTN_STOP = 18;
 const byte POTENTIOMETER = 35;
-const byte MOTOR = 25;
-const byte SCK_PIN = 14;
-const byte CS_PIN = 27;
-const byte SO_PIN = 26;
+const byte MOTOR = 33;
+const byte SCK_PIN = 27;
+const byte CS_PIN = 26;
+const byte SO_PIN = 25;
 
 // Define Wi-Fi credentials
 const String WIFI_SSID = "<WIFI NAME>";
@@ -72,10 +73,21 @@ void setup() {
 
         // Initialize pin modes
         pinMode(LED_GREEN, OUTPUT);
+        pinMode(LED_YELLOW, OUTPUT);
         pinMode(LED_RED, OUTPUT);
         pinMode(BTN_START, INPUT_PULLUP);
         pinMode(BTN_STOP, INPUT_PULLUP);
         pinMode(MOTOR, OUTPUT);
+
+        // Turn off LEDs
+        digitalWrite(LED_GREEN, HIGH);
+        digitalWrite(LED_YELLOW, HIGH);
+        digitalWrite(LED_RED, HIGH);
+        delay(1800);
+
+        digitalWrite(LED_GREEN, LOW);
+        digitalWrite(LED_YELLOW, LOW);
+        digitalWrite(LED_RED, LOW);
 
         // Attach interrupts
         attachInterrupt(
@@ -83,75 +95,61 @@ void setup() {
             stopMachine, FALLING);
         attachInterrupt(
             digitalPinToInterrupt(BTN_START), 
-            startMachine, FALLING);
-
-        // Turn off LEDs
-        digitalWrite(LED_GREEN, HIGH);
-        digitalWrite(LED_RED, HIGH);
-        delay(1800);
-
-        digitalWrite(LED_GREEN, LOW);
-        digitalWrite(LED_RED, LOW);
-    }
+            startMachine, FALLING);    
+    }    
 }
 
 void loop() {
+
     if (WiFi.status() != WL_CONNECTED) {
         Serial.println("No internet connection...");
         delay(1000);
         return;
     }
 
-    // Retrieve sensor data
-    sensor = http.retrieveSensors();
-    delay(10);
-
     // Check if the machine is initialized and started
     if (sensor.is_initialized && sensor.is_start) {
-        digitalWrite(LED_GREEN, HIGH);
+        digitalWrite(LED_YELLOW, HIGH);
         
+        if (!state.equals(START)) return;
+
+        digitalWrite(LED_YELLOW, LOW);
+        digitalWrite(LED_GREEN, HIGH);
+
         // Run machine while the state is "START"
         while (state.equals(START)) {
-            Serial.flush();
-
             // Get motor speed from potentiometer
             int value = map(analogRead(POTENTIOMETER), 0, 4095, 0, 255);  
             analogWrite(MOTOR, value);
-
+            delay(10);
+ 
             // Read temperature
             sensor.temperature = thermo.readCelsius();
-            delay(10);
 
+            // Check if machine should stop
+            if (sensor.is_stop) break;
+            
             // Update sensor values
             http.updateSensors(sensor);       
 
             // Check if machine should stop
-            if (sensor.is_stop) {
-                state = STOP;
-                break;
-            }
+            if (sensor.is_stop) break;
 
             // Read sensor data
             sensor = http.retrieveSensors();
 
             // Check if machine should stop
-            if (sensor.is_stop) {
-                state = STOP;
-                break;
-            }
-
-            delay(10);
+            if (sensor.is_stop) break;
         }
 
-        // Check if machine is not stopped prematurely
-        if (!sensor.is_stop) return;
-
     } else {
+        sensor = http.retrieveSensors();
         return;
     }
         
     // Stop the machine
-    if (state.equals(STOP) || sensor.is_stop) {
+    if (state.equals(STOP) || sensor.is_stop) { // update the condition 
+
         analogWrite(MOTOR, 0);
         digitalWrite(LED_GREEN, LOW);
         digitalWrite(LED_RED, HIGH);
@@ -160,11 +158,8 @@ void loop() {
 
         // Reset sensor values
         state = NONE; 
-        sensor.is_start = false;
         sensor.is_initialized = false;
-        sensor.counter = 0;
-        sensor.timer = 0;
-        sensor.temperature = 0;
+        sensor.is_start = false;
         sensor.is_stop = false;
         http.updateSensors(sensor);
     }
@@ -173,8 +168,8 @@ void loop() {
 // Interrupt service routine for stop button
 void stopMachine() {
     if (state.equals(START)) {
+        state = STOP;
         sensor.is_stop = true;
-        Serial.println("Stop button pressed.");
     }
 }
 
@@ -183,6 +178,5 @@ void startMachine() {
     if (state.equals(NONE) && sensor.is_start && sensor.is_initialized) {
         state = START;
         sensor.is_stop = false;
-        Serial.println("Start button pressed.");
     }
 }
