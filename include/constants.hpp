93cc51fff47978c851aa210258a5b9b0a5e6372f
Lirae Que Data <liraedata59@gmail.com>
2024-05-15 02:50:40 +0800

    Add wifi credential constants

--- include/constants.hpp
+++ include/constants.hpp
@@ -7,6 +7,10 @@
 // Machine UUID
 const String UUID = "2ae09d7a-8f5d-496f-9917-4deabb3e5ad3";
 
+// Wifi credentials
+const String WIFI_SSID = "WIFI_SSID_HERE";
+const String WIFI_PASSWORD = "WIFI_PASSWORD_HERE";
+
 // LED pins
 const byte LED_RED = 14;
 const byte LED_YELLOW = 12; 
