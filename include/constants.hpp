5f77d093a1e518881b41cdf0f4ccb2676d9aa528
Lirae Que Data <liraedata59@gmail.com>
2024-05-15 02:46:43 +0800

    Add LED constant variable

--- include/constants.hpp
+++ include/constants.hpp
@@ -7,6 +7,10 @@
 // Machine UUID
 const String UUID = "2ae09d7a-8f5d-496f-9917-4deabb3e5ad3";
 
+// LED pins
+const byte LED_RED = 14;
+const byte LED_YELLOW = 12; 
+const byte LED_GREEN = 13;
 
 
 #endif
