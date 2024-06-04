38be88101818aafec9a2725016ccfa946025c265
Lirae Que Data <liraedata59@gmail.com>
2024-06-05 01:31:17 +0800

    Add displayLCD method implementation

--- lib/ESPScreen/ESPScreen.cpp
+++ lib/ESPScreen/ESPScreen.cpp
@@ -11,3 +11,12 @@ void initLCD() {
     lcd.printf(">> MakerSync! <<");
     delay(2500);
 }
+
+
+void displayLCD(const char * line1, const char* line2) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.printf(line1);
+    lcd.setCursor(0, 1);
+    lcd.printf(line2);
+}
